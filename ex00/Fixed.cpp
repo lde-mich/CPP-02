ebde5dbfbf96e25c769f4b4dd4f55d2f9dccc834
lde-mich <luca01112003@gmail.com>
2023-12-22 17:48:45 +0100

    upload-dec-22

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,42 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 18:46:36 by lde-mich          #+#    #+#             */
-/*   Updated: 2023/10/30 18:46:37 by lde-mich         ###   ########.fr       */
+/*   Updated: 2023/12/22 17:47:10 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    std::cout<<"Default constructor called"<< std::endl;
+    this->numberFixed = 0;
+    this->nBit = 8;
+}
+
+Fixed::Fixed(Fixed const &o)
+{
+    this->numberFixed = o.numberFixed;
+    this->nBit = o.nBit;
+    std::cout<<"Copy constructor called"<< std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout<<"Destructor called"<< std::endl;
+}
+
+void Fixed::operator = (Fixed const &fixed)
+{
+    std::cout<<"Copy assignment operator called"<< std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout<<"getRawBits member function called"<< std::endl;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    
+}
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,23 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 18:46:40 by lde-mich          #+#    #+#             */
-/*   Updated: 2023/10/30 18:46:41 by lde-mich         ###   ########.fr       */
+/*   Updated: 2023/12/22 17:45:22 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int numberFixed;
+		static int nBit;
+
+	public:
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		void operator = (Fixed const &fixed);
+		Fixed(Fixed const &o);
+		Fixed();
+		~Fixed();
+};
